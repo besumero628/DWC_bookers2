fc3b748fff38e8f5c8c92bae108a2dabf1e763de
besumero628 <besumero628@gmail.com>
2021-07-06 14:51:08 +0000

    [Update] header link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  
-  resources :users, only: [:index, :edit]
+
+  resources :users, only: [:index, :show, :edit]
+  resources :books, except: [:new]
 end

525d3054586134581fbc68a3c7191300c51a44a5
besumero628 <besumero628@gmail.com>
2021-07-08 01:49:24 +0000

    [Add] homes about view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get "homes/about" => "homes#about", as: :homes_about
+  
   devise_for :users
 
   resources :users, only: [:index, :show, :edit]

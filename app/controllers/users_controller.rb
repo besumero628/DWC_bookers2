4ccf11d597b336a665c361eab44aeb5e7be384a1
besumero628 <besumero628@gmail.com>
2021-07-10 07:26:37 +0000

    [Update] book edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  
+
   def index
     @user = User.find(current_user.id)
     @users = User.all
     @book = Book.new #new book
   end
-  
+
   def show
     @user = User.find(params[:id])
     @users = User.all
@@ -16,16 +16,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(params[:id])
+    if user.update(user_params)
+      flash.notice = "You have updated user successfully."
+      redirect_to user_path(user.id)
+    else
+      @user = user
+      render action: :edit
+    end
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+
+  validates :name, presence:true
+
   attachment :profile_image
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :books, dependent: :destroy
 end

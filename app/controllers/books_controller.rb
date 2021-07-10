0eae42f92430d01472daf5037abbf7daab860f87
besumero628 <besumero628@gmail.com>
2021-07-10 06:22:02 +0000

    [Update] book edit page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,10 @@ class BooksController < ApplicationController
      @user = User.find(@books.user_id)
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def create
     book = Book.new(book_params)
     book.user_id = current_user.id
@@ -26,6 +30,26 @@ class BooksController < ApplicationController
     end
   end
 
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash.notice = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      @book = book
+      render action: :edit
+    end
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash.notice = "You have destroyed book successfully."
+    redirect_to books_path
+  end
+
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
     redirect_to user_path(params[:id])
   end
   
+  private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end

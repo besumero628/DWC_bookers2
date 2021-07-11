f4c2c288e882bdb2dc47fc168ca9a7ae3a760d1a
besumero628 <besumero628@gmail.com>
2021-07-11 03:28:39 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
   before_action :ensure_current_user, {only: [:edit, :update]}
-  
+
   def ensure_current_user
     @user_id = Book.find(params[:id]).user_id
     if current_user.id != @user_id
       flash.alert = "You cannot access!"
-      redirect_to user_path(current_user.id)
+      redirect_to books_path
     end
   end
 
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
     book.user_id = current_user.id
     if book.save
       flash.notice = "You have created book successfully."
-      redirect_to user_path(current_user.id) #books/showに変える
+      redirect_to book_path(book.id)
     else
       @user = User.find(current_user.id)
       @users = User.all

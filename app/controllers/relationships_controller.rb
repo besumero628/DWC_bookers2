31e4d79b2327c17d6894bf2e7ce23180be6ff0f7
besumero628 <besumero628@gmail.com>
2021-07-29 03:26:05 +0000

    [Update] migration

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,11 @@
 class RelationshipsController < ApplicationController
+  
+  def create
+    
+  end
+  
+  def destroy
+    
+  end
+  
 end
--- db/migrate/20210729000631_create_relationships.rb
+++ db/migrate/20210729000631_create_relationships.rb
@@ -1,8 +1,13 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
+      t.references :follower, foreign_key: true
+      t.references :followed, foreign_key: true
 
       t.timestamps
     end
+    
+    add_foreign_key :relationships, :users, column: :follower_id
+    add_foreign_key :relationships, :users, column: :followed_id
   end
 end

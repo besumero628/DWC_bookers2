735f4398b8d9b0b7c3a5f3b5b5630664b38ca56d
besumero628 <besumero628@gmail.com>
2021-08-02 08:04:40 +0000

    [Restart] search window add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" => "homes#about", as: :home_about
-  
+
   devise_for :users
 
   resources :users, only: [:index, :show, :edit, :update] do
@@ -9,10 +9,12 @@ Rails.application.routes.draw do
     get "follower" => "relationships#follower", as: :relationships_follower
     get "followed" => "relationships#followed", as: :relationships_followed
   end
-  
+
   resources :books, except: [:new] do
     resources :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
+  resources :searches, only:[:index, :create]
+
 end
